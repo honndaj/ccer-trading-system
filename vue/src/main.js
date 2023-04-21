1727419a12d281c7f46ff89acb9d5e7ede821860
honndaj <lizeqing0@gmail.com>
2023-04-21 17:40:26 +0800

    update el-button color

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import './assets/gloable.css'
+import './assets/global_style.css'
 import request from "@/utils/request";
 import store from './store'
 
